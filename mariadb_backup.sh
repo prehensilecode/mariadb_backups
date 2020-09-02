b9227fbbc06b2568c15089b3a92c42d4331fae54
David Chin <david.w.h.chin@gmail.com>
2020-09-01 23:03:31 -0400

    First combined version

---
+++ mariadb_backup.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+###
+### MariaDB backup
+###
+
+set -e
+set -u
+set -o pipefail
+
+function help {
+    echo "script usage: $(basename $0) [-t full | incr]" >&2
+}
+
+if [ $# -eq 0 ]
+then
+    help
+    exit 1
+else
+    while getopts "t:" OPTION
+    do
+        case "$OPTION" in
+            t)
+                tvalue="$OPTARG"
+                echo "The value provided is $OPTARG"
+                echo "tvalue = $tvalue"
+                ;;
+            ?)
+                help
+                exit 1
+                ;;
+        esac
+    done
+fi
+
+shift "$(($OPTIND -1))"
+
+MARIABACKUP=/bin/mariabackup
+BACKUP_BASE_DIR=/var/cache/mariabackup
+
+# delete backup directories older than 14 hours
+find ${BACKUP_BASE_DIR}/* -type d -mmin +$((60*14)) -exec rm -rf {} \;
+
+MONTH_DIR=${BACKUP_BASE_DIR}/`date +%Y-%m`
+TARGET_DIR=${MONTH_DIR}/$( date +%d-%H%M-${tvalue} )
+
+MARIABACKUP_OPTS="--backup --target-dir=${TARGET_DIR} --user=mariabackup --password=some_password"
+
+LOG_DIR=/var/log/mariabackup
+LOG=${LOG_DIR}/mariadb_backup_full.log
+
+if [[ -e ${TARGET_DIR} ]]
+then
+    printf "[`date --rfc-3339=seconds --utc`] ERROR - Directory ${TARGET_DIR} already exists\n" >> ${LOG}
+    exit 1
+else
+    if [[ x${tvalue} = xfull ]]
+    then
+        mkdir -p ${TARGET_DIR}
+
+        SECONDS=0
+
+        ${MARIABACKUP} ${MARIABACKUP_OPTS} >> ${LOG} 2>&1
+    
+        printf "[`date --rfc-3339=seconds --utc`] INFO - Completed in ${SECONDS} seconds\n" >> ${LOG}
+    
+        printf ${TARGET_DIR} > ${MONTH_DIR}/last_completed_backup
+        exit 0
+    elif [[ x${tvalue} = xincr ]]
+    then
+        mkdir -p ${MONTH_DIR}
+
+        if [[ -e ${MONTH_DIR}/last_completed_backup ]]
+        then
+            BASE_DIR=$(head -n 1 ${MONTH_DIR}/last_completed_backup)
+
+            if [[ -z ${BASE_DIR} ]]
+            then
+                printf "[`date --rfc-3339=seconds --utc`] ERROR - Base dir is an empty string\n" >> ${LOG}
+            else
+                mkdir -p ${TARGET_DIR}
+
+                SECONDS=0
+
+                ${MARIABACKUP} ${MARIABACKUP_OPTS} \
+                    --incremental-basedir=${BASE_DIR}  >> ${LOG} 2>&1
+
+                printf "[`date --rfc-3339=seconds --utc`] INFO - Completed in ${SECONDS} seconds\n" >> ${LOG}
+
+                printf ${TARGET_DIR} > ${MONTH_DIR}/last_completed_backup
+                exit 0
+            fi
+        else
+            printf "[`date --rfc-3339=seconds --utc`] ERROR - No base dir for incremental backup\n" >> ${LOG}
+            exit 3
+        fi
+    fi
+fi
+
